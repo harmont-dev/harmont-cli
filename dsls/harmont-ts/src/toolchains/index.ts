e1714c8bde036622786e6dfd5a8a636f338dea77
Marko Vejnovic <marko@harmont.dev>
2026-05-23 22:20:25 -0700

    feat(ts-dsl): export all language toolchains from barrel

--- dsls/harmont-ts/src/toolchains/index.ts
+++ dsls/harmont-ts/src/toolchains/index.ts
@@ -1,1 +1,28 @@
 export { npm, NpmProject, type NpmOptions } from "./npm.js";
+export { go, GoToolchain, type GoOptions } from "./go.js";
+export { rust, RustToolchain, type RustOptions } from "./rust.js";
+export { python, PythonToolchain, type PythonOptions } from "./python.js";
+export { cmake, CMakeProject, type CMakeOptions } from "./cmake.js";
+export { gradle, GradleProject, type GradleOptions } from "./gradle.js";
+export { dotnet, DotnetProject, type DotnetOptions } from "./dotnet.js";
+export { ruby, RubyProject, type RubyOptions } from "./ruby.js";
+export { perl, PerlProject, type PerlOptions } from "./perl.js";
+export {
+  composer,
+  ComposerProject,
+  type ComposerOptions,
+} from "./composer.js";
+export { elm, ElmProject, type ElmOptions } from "./elm.js";
+export {
+  zig,
+  ZigToolchain,
+  ZigProject,
+  type ZigOptions,
+} from "./zig.js";
+export { ocaml, OCamlProject, type OCamlOptions } from "./ocaml.js";
+export {
+  haskell,
+  HaskellToolchain,
+  HaskellPackage,
+  type HaskellOptions,
+} from "./haskell.js";
